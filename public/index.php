2e5e91459d43b6e87b83830b680a74c231d3e838
andywestley <andy@andrewwestley.co.uk>
2025-12-11 15:46:15 +0000

    refactor: Remove session_start() from index.php for improved structure

--- public/index.php
+++ public/index.php
@@ -5,8 +5,6 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-session_start();
-
 // --- VIEW RENDERING LOGIC ---
 
 // Determine which screen to display based on the session state.
