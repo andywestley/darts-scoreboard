32412e70b51c402a00684a30db13cd885599a91f
andywestley <andy@andrewwestley.co.uk>
2025-12-12 10:58:21 +0000

    More of the same bug

--- bootstrap.php
+++ bootstrap.php
@@ -33,6 +33,7 @@ register_shutdown_function(function () {
 });
 
 // JWT Library - Manually included instead of using Composer
+require_once __DIR__ . '/src/lib/php-jwt/JWTExceptionWithPayloadInterface.php';
 require_once __DIR__ . '/src/lib/php-jwt/ExpiredException.php';
 require_once __DIR__ . '/src/lib/php-jwt/SignatureInvalidException.php';
 require_once __DIR__ . '/src/lib/php-jwt/BeforeValidException.php';
